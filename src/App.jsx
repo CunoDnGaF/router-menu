487280989653cb03e61e489a488c25037cb1a306
Nimat13 <nikitamatrosov4@gmail.com>
2024-02-01 17:28:32 +0300

    Задание выполнено

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Menu from './components/Menu/Menu';
+import HomePage from './components/HomePage/HomePage';
+import DriftPage from './components/DriftPage/DriftPage';
+import ForzaPage from './components/ForzaPage/ForzaPage';
+import TimeAttackPage from './components/TimeAttackPage/TimeAttackPage';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <Menu />
+        <div className="page">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/drift" element={<DriftPage />} />
+            <Route path="/timeattack" element={<TimeAttackPage />} />
+            <Route path="/forza" element={<ForzaPage />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
